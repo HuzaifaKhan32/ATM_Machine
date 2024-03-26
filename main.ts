7c8574f024541f1c5a836481c87cad050e662772
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2024-03-27 03:48:46 +0500

    Adding files

---
+++ main.ts
@@ -0,0 +1,78 @@
+import inquirer from "inquirer";
+
+let myBalance: number = 10000;
+
+
+const atmPin: number = 2006;
+
+let answerPin = await inquirer.prompt
+    (
+        [
+            {
+                name: "pin",
+                message: "Enter Your four digit pin: ",
+                type: "number"
+            }])
+
+if (answerPin.pin === atmPin) {
+    console.log("Your Pin Is Correct!!!");
+    let doOperation = await inquirer.prompt
+        (
+            [
+                {
+                    name: "action",
+                    message: "What do you want to do?",
+                    type: "list",
+                    choices: ["Withdraw", "Check Balance"]
+                }])
+    if (doOperation.action == "Withdraw") {
+        let amount = await inquirer.prompt([{
+            name: "amount",
+            message: "Select amount which you want to withdraw:",
+            type: "list",
+            choices: ["500", "1000", "5000", "10000", "25000", "50000"]
+        }])
+        if (amount.amount > myBalance) {
+            console.log(`You don't have enough amount. Your current balance is: ${myBalance}`);
+        } else if (amount.amount == "500") {
+            myBalance -= amount.amount
+
+        } else if (amount.amount == "1000") {
+            myBalance -= amount.amount
+
+        } else if (amount.amount == "5000") {
+            myBalance -= amount.amount
+
+        } else if (amount.amount == "10000") {
+            myBalance -= amount.amount
+
+        } else if (amount.amount == "25000") {
+            myBalance -= amount.amount
+
+        } else {
+            myBalance -= amount.amount
+
+        }
+        let passCash = await inquirer.prompt
+            (
+                [
+                    {
+                        name: "cash",
+                        message: "Do you want to pass your cash",
+                        type: "list",
+                        choices: ["yes", "no"]
+                    }])
+        if (passCash.cash == "yes") {
+            console.log(`Receive your Rs.${amount.amount} cash, Thank you for your transaction :)`);
+            console.log(`Your remaining balance after transaction is: ${myBalance}`);
+        } else {
+            console.log("Thank You!, Your transaction has been returned back.");
+        }
+
+    }
+    if (doOperation.action == "Check Balance") {
+        console.log(`Your current balance is: ${myBalance}`);
+    }
+} else {
+    console.log("Incorrect Pin. Please enter correct pin.");
+}
